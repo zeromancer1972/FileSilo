128344db1d37eb252af125d69537b0d31189dbbf
zeromancer1972 <obusse@googlemail.com>
2014-02-12 20:58:35 +0100

    Added log for every action

---
+++ FileSilo ODP/Code/Java/com/olb/Log.java
@@ -0,0 +1,35 @@
+package com.olb;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import lotus.domino.DateTime;
+import lotus.domino.Document;
+
+import com.ibm.xsp.extlib.util.ExtLibUtil;
+
+public class Log implements Serializable {
+
+	private static final long serialVersionUID = -3332856685880091459L;
+
+	public Log() {
+
+	}
+
+	public void add(String subject, String message) {
+		try {
+			Document doc = ExtLibUtil.getCurrentDatabase().createDocument();
+			doc.replaceItemValue("Form", "log");
+			doc.replaceItemValue("$PublicAccess", "1");
+			DateTime now = ExtLibUtil.getCurrentSession().createDateTime(new Date());
+			doc.replaceItemValue("logTimestamp", now);
+			doc.replaceItemValue("logSubject", subject);
+			doc.replaceItemValue("logMessage", message);
+			doc.save(true, false);
+			doc.recycle();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
