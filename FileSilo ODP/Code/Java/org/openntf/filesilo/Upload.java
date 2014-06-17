9745c3beba2cdd0ae2cdfa80fd4dcd9da808edb5
zeromancer1972 <obusse@googlemail.com>
2014-06-17 20:59:08 +0200

    Implementation of Pushover service

--- FileSilo ODP/Code/Java/org/openntf/filesilo/Upload.java
+++ FileSilo ODP/Code/Java/org/openntf/filesilo/Upload.java
@@ -1,5 +1,6 @@
 package org.openntf.filesilo;
 
+import java.io.IOException;
 import java.io.Serializable;
 
 import lotus.domino.Database;
@@ -8,7 +9,10 @@ import lotus.domino.NotesException;
 import lotus.domino.Session;
 import lotus.domino.View;
 
+import org.apache.http.client.ClientProtocolException;
 import org.openntf.domino.utils.XSPUtil;
+
+import com.ibm.commons.util.io.json.JsonException;
 
 public class Upload implements Serializable {
 	/**
@@ -21,6 +25,11 @@ public class Upload implements Serializable {
 	private String adr = "";
 	private String msg = "A new upload has been posted in <DB>";
 	private int adjust = 30;
+
+	// Pushover
+	private String userToken;
+	private String appToken;
+	private String url;
 
 	public Upload() {
 		String dbtitle = "";
@@ -37,17 +46,32 @@ public class Upload implements Serializable {
 			}
 			this.profile = doc;
 			this.adr = doc.getItemValueString("profileMail");
-			this.msg = doc.getItemValueString("profileMessage");
+			if (!doc.getItemValueString("profileMessage").equals("")) {
+				this.msg = doc.getItemValueString("profileMessage");
+			}
+			this.msg = this.msg.replace("<DB>", this.db.getTitle());
 			this.adjust = doc.getItemValueInteger("profileAdjust");
+
+			// Pushover
+			this.userToken = doc.getItemValueString("profileUserToken");
+			this.appToken = doc.getItemValueString("profileAppToken");
+			this.url = doc.getItemValueString("profileURL");
+
 			doc.recycle();
 		} catch (NotesException e) {
-			System.out.println("FileSilo: please provide a profile settings document in " + dbtitle + ", " + dbpath);
+			System.out.println("FileSilo: please provide a profile settings document in " + dbtitle
+					+ ", " + dbpath);
 		}
+	}
+
+	public void setUrl(final String url) {
+		this.url = url;
 	}
 
 	public void sendInfo() {
 		if (this.profile == null)
 			return;
+		sendPushover();
 		try {
 			Document mail = this.db.createDocument();
 			if (this.adr.equals(""))
@@ -61,7 +85,32 @@ public class Upload implements Serializable {
 		} catch (NotesException e) {
 
 		}
+	}
 
+	public void sendPushover() {
+		if (this.profile == null)
+			return;
+
+		Pushover p = new Pushover();
+		p.setUserToken(this.userToken);
+		p.setAppToken(this.appToken);
+		p.setMessage(this.msg);
+		p.setUrl(this.url);
+		try {
+			p.send();
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (JsonException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public int getAdjust() {
