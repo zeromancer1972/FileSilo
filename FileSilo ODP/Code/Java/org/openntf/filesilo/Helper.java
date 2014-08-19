fbd19606554d291af7c014f1268092cf168ada88
zeromancer1972 <obusse@googlemail.com>
2014-08-19 23:47:05 +0200

    Simplification of size computation, codename "September"

--- FileSilo ODP/Code/Java/org/openntf/filesilo/Helper.java
+++ FileSilo ODP/Code/Java/org/openntf/filesilo/Helper.java
@@ -43,12 +43,7 @@ public class Helper implements Serializable {
 		for (int x = 0; x < size.length; x++) {
 			bytes += size[x];
 		}
-		int unit = si ? 1000 : 1024;
-		if (bytes < unit)
-			return bytes + " B";
-		int exp = (int) (Math.log(bytes) / Math.log(unit));
-		String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
-		return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+		return humanReadableByteCount(bytes, si);
 	}
 
 }
